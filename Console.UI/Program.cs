390fd2bb53103a1e3eea0ed88df8d02dbdbf2fc5
Navid <navidjalili3@gmail.com>
2023-07-28 17:02:04 +0330

    Add main program logic for memory management simulation

---
+++ Console.UI/Program.cs
@@ -0,0 +1,14 @@
+﻿using FreeSpaceManagement.Library;
+
+FreeSpaceManager manager = new FreeSpaceManager();
+int totalMemorySize = 100; // Total memory size in units
+manager.Initialize(totalMemorySize);
+manager.DisplayMemoryStatus();
+manager.Allocate(30);
+manager.Allocate(20);
+manager.Allocate(10);
+manager.DisplayMemoryStatus();
+manager.Deallocate(30, 60);
+manager.DisplayMemoryStatus();
+manager.Allocate(25);
+manager.DisplayMemoryStatus();
